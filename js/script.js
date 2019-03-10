7bb9c7b5843beb5b96396a8195c0f166d985f805
roxey804 <32243415+roxey804@users.noreply.github.com>
2019-03-10 22:12:33 +0000

    updated website

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+// Display on click for Team building activities
+$("#clickhere").click(function() {
+$("#teambuildinfo").slideDown();
+});
+$("#teambuildinfo").click(function(){
+    $("#teambuildinfo").slideUp();
+  }); 
+    });
+
+
+// Display on click for Parties
+
+
+$("#clickhere2").click(function() {
+$("#partyinfo").slideDown();
+});
+$("#partyinfo").click(function(){
+    $("#partyinfo").slideUp();
+  });
+
+
+// Display on click for Community events
+
+
+  $("#clickhere3").click(function() {
+  $("#communityinfo").slideDown();
+  });
+  $("#communityinfo").click(function(){
+      $("#communityinfo").slideUp();
+    });
